84b6b59d2ed76e7478e3ca696f658941d78c3d4d
Joe Schrag <gh@joeschrag.com>
2025-03-30 21:06:04 -0600

    Added text aggregator

--- test_config.py
+++ test_config.py
@@ -3,15 +3,22 @@ import yaml
 import tempfile
 import os
 from typing import List
-from config import load_config_from, TranscriptionSetConfig, OutputConfig  # Replace 'your_module' with the actual module name
+from config import load_config_from, OutputConfig, AggregationConfig  # Replace 'your_module' with the actual module name
 
 def test_load_yaml():
-    transaction_sets = load_config_from("config.yaml")
-
-    assert len(transaction_sets) == 5
-    assert len(transaction_sets[0].audio_inputs) == 5
-    assert len(transaction_sets[0].text_outputs) == 3
+    config = load_config_from("config.yaml")
+    ts = config.transcription_sets
     
-    assert isinstance(transaction_sets[0].text_outputs[1], OutputConfig)
-    assert transaction_sets[0].text_outputs[1].directory == "/mnt/documents/Transcription/Journal"
-    assert transaction_sets[0].text_outputs[1].keyword == "journal"
+    assert len(ts) == 5
+    assert len(ts[0].audio_inputs) == 5
+    assert len(ts[0].text_outputs) == 3
+    
+    assert isinstance(ts[0].text_outputs[1], OutputConfig)
+    assert ts[0].text_outputs[1].directory == "/mnt/documents/Transcription/Journal"
+    assert ts[0].text_outputs[1].keyword == "journal"
+    
+    aggs = config.aggregations
+    assert len(aggs) == 1
+    assert isinstance(aggs[0], AggregationConfig)
+    assert aggs[0].input_directory == "/mnt/documents/Transcription/Standup"
+    assert aggs[0].output_filepath == "/mnt/documents/Transcription/StandupAggregation.txt"

b6713a40bbb9f40a7a0bfcdd0bce74885948d719
Joe Schrag <gh@joeschrag.com>
2025-09-23 23:04:08 -0600

    Updated README

--- file_system_utils.py
+++ file_system_utils.py
@@ -7,7 +7,6 @@ class FileSystemUtils:
         with os.scandir(directory) as entries:
             files = [(entry.name, entry.stat().st_mtime) for entry in entries if entry.is_file() and entry.name.endswith(('.mp3', '.wav', '.m4a', '.flac'))]
         
-        # Sort files by modified time (newest first)
         files.sort(key=lambda x: x[1], reverse=True)
         
         return [file[0] for file in files]  # Return only filenames
